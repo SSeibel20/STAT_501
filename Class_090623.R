82aa5807add4e62321d3c7b67ae58de9874f9642
Samantha Seibel <sls6550@psu.edu>
2023-09-06 13:11:49 -0400

    edits

--- Class_090623.R
+++ Class_090623.R
@@ -1,6 +1,8 @@
 #Class 09/06/2023
 #Samantha Seibel
 
+library(EnvStats)
+
 #determine working directory
 getwd()
 
@@ -8,6 +10,14 @@ getwd()
 sc <- read.table("Intro_data.csv", sep=",", header=T)
 
 #write a linear model
-model <- lm(y~x, data=sc)
+model <- lm(x~y, data=sc)
 
-#
+#get a 95% confidence interval for slope and intercept
+confint(model, level=0.95)
+
+#get summary of the linear model
+summary(model)
+
+#run anocv anad lack of fit anova
+anova(model)
+anovaPE(model)
